f04fc3cadeac52ba902abe84350bf0a602256edd
kiwavi <dekamotho@gmail.com>
2023-02-17 09:33:00 +0000

    Added configuration necessary for a production environment

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -8,7 +8,7 @@ fi
 domains=(yanarana.com www.yanarana.com)
 rsa_key_size=4096
 data_path="./docker/nginx/certbot"
-email="rotpil101@gmail.com" # Adding a valid address is strongly recommended
+email="kainangone12@gmail.com" # Adding a valid address is strongly recommended
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
